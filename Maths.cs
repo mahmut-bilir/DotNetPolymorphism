ec3c2332bb4a540c6da09df51e155d9367ca8110
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-28 10:36:00 +0300

    Compile time polymorphism(Method overloading)

---
+++ Maths.cs
@@ -0,0 +1,18 @@
+public class Maths
+{
+
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Sum(double a, double b)
+    {
+        return a + b;
+    }
+
+    public int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Maths maths = new Maths();
+
+Console.WriteLine(maths.Sum(2, 5));
+Console.WriteLine(maths.Sum(1.3, 3.6));
+Console.WriteLine(maths.Sum(3, 2, 4));
