63b75a54f9ab685311090e2f9d0282cdc048d41f
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-28 11:25:40 +0300

    Polymorphism with abstract class

--- Animal.cs
+++ Animal.cs
@@ -1,7 +1,5 @@
-public class Animal
+public abstract class Animal
 {
-    public virtual void MakeSound()
-    {
-        Console.WriteLine("The animal makes a sound.");
-    }
+    public abstract void MakeSound();
+
 }
